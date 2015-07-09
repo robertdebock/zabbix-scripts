9d2a02b25813a43bc69e6b4f3736efd186f7c091
Robert de Bock <robert@meinit.nl>
2015-07-09 20:47:20 +0200

    Update tcpport_lld.sh

--- tcpport_lld.sh
+++ tcpport_lld.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# A script to report all open ports on a host. Requires the package "nmap" to be installed.
+
+# Zabbix needs to be configured in order to use this script:
+# Discovery:
+# Name: Open TCP ports
+# Type: External check
+# Key: zabbix_tcpport_lld.sh[{HOST.CONN}]
+# (This makes the variable {#PORT} and {#PROTO} available for use in the items and triggers.)
+
+# Item Prototypes
+# Name: Status of port {#PORT}/{#PROTO}
+# Type: Simple check
+# Key: net.tcp.service[{#PROTO},,{#PORT}]
+# Type of information: Numeric (unsigned)
+# Data type: Boolean
+
+# Trigger Prototypes
+# Name: {#PROTO} port {#PORT}
+# Expression: {Template_network:net.tcp.service[{#PROTO},,{#PORT}].last(0)}=0
+
 echo '{'
 echo ' "data":['
 
